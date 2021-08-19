787803bb27d250c2e545c26992ff131b74d118d8
AEcalle <contact@aurelienecalle.fr>
2021-08-19 16:15:10 +0200

    Add run function index.php

--- public/index.php
+++ public/index.php
@@ -9,3 +9,5 @@ $router = new Router($_GET['url']);
 $router->get('/posts',function(){echo 'Tous les articles';});
 $router->get('/posts/:id',function($id){echo 'Afficher l\'article '. $id;});
 $router->post('/posts/:id',function($id){echo 'Poster pour l\'article '. $id;});
+
+$router->run();
