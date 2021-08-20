347471324719c228f2157f35fc0ac18b6aaab997
AEcalle <contact@aurelienecalle.fr>
2021-08-20 10:44:00 +0200

    Add named routes

--- public/index.php
+++ public/index.php
@@ -8,8 +8,9 @@ $router = new Router($_GET['url']);
 
 $router->get('/',function(){echo 'Homepage';});
 $router->get('/posts',function(){echo 'Tous les articles';});
-$router->get('/posts/:id-:slug',function($id, $slug){echo 'Afficher l\'article '
-    .$slug.':'. $id;})->width('id','[0-9]+')->width('slug','[0-9a-z\-]+');
+$router->get('/posts/:id-:slug',function($id, $slug) use ($router)
+{echo $router->url('posts.show',['id'=>$id,'slug'=>$slug]);},
+'posts.show')->width('id','[0-9]+')->width('slug','[0-9a-z\-]+');
 $router->get('/posts/:id',function($id){echo 'Afficher l\'article '.$id;});
 $router->post('/posts/:id',function($id){echo 'Poster pour l\'article '. $id;});
 
--- src/Router/Router.php
+++ src/Router/Router.php
@@ -2,32 +2,40 @@
 
 namespace AEcalle\Oc\Php\Project5\Router;
 
+use Exception;
+
 class Router
 {
 
     private $url;
     private $routes = [];
+    private $namedRoutes = [];
 
     public function __construct($url)
     {
         $this->url = $url;
     }
 
-    public function get($path, $callable)
+    public function get($path, $callable, $name = null)
     {
-        $route = new Route($path, $callable);
-        $this->routes['GET'][] = $route;
-
-        return $route;
+        return $this->add($path, $callable, $name, 'GET');
     }
 
-    public function post($path, $callable)
+    public function post($path, $callable, $name = null)
+    {
+        return $this->add($path, $callable, $name, 'POST');
+    }
+    
+    private function add($path, $callable, $name, $method)
     {
         $route = new Route($path, $callable);
-        $this->routes['POST'][] = $route;
-
+        $this->routes[$method][] = $route;
+        if($name)
+        {
+            $this->namedRoutes[$name] = $route;
+        }
         return $route;
-    }    
+    }
 
     public function run()
     {
@@ -44,5 +52,15 @@ class Router
         }
 
         throw new RouterException('No matching routes');
+    }
+
+    public function url($name,$params = [])
+    {
+        if(!isset($this->namedRoutes[$name]))
+        {
+            throw new RouterException('No route matches this name');
+        }
+
+        return $this->namedRoutes[$name]->getUrl($params);
     }
 }
