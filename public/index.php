9428fe90b408d069c3e32ad1f84d554f8b81b167
AEcalle <contact@aurelienecalle.fr>
2021-08-25 15:11:32 +0200

    Links Modified

--- public/index.php
+++ public/index.php
@@ -10,9 +10,9 @@ $router = new Router($request->getPathInfo());
 
 $router->get('/','FrontController#home');
 $router->get('/blog',function(){echo 'Affiche tous les articles';});
-$router->get('/posts/:id-:slug',function($id,$slug)
+$router->get('/post/:id-:slug',function($id,$slug)
 {echo $id.' : '.$slug;},'posts.show')->width('id','[0-9]+')->width('slug','[0-9a-z\-]+');
-$router->post('/posts/:id-:slug',function($id,$slug){echo 'Poster un commentaire pour l\'article '. $id;});
+$router->post('/post/:id-:slug',function($id,$slug){echo 'Poster un commentaire pour l\'article '. $id;});
 $router->get('/login',function(){echo 'Se connecter';});
 $router->get('/legal',function(){echo 'Mentions légales';});
 
