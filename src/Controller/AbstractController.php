e5cbd1487a6b34801a028f4cee9a78147db1f954
AEcalle <contact@aurelienecalle.fr>
2021-08-25 13:54:41 +0200

    Add function render to AbstractController

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,7 +2,16 @@
 
 namespace AEcalle\Oc\Php\Project5\Controller;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 abstract class AbstractController
 {
+    public function render(string $view, array $context = [])
+    {
+        $loader = new FilesystemLoader('../templates');
+        $twig = new Environment($loader);
 
+        return $twig->render($view, $context);
+    }
 }
