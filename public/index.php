9eaa58b674823e6f30931db08965a67f41b8941e
AEcalle <contact@aurelienecalle.fr>
2021-09-02 10:16:28 +0200

    Add homepage post route

--- public/index.php
+++ public/index.php
@@ -9,13 +9,14 @@ $request = Request::createFromGlobals();
 $router = new Router($request->getPathInfo());
 
 $router->get('/','FrontController#home','home');
+$router->post('/','FrontController#home','home');
 $router->get('/blog',function(){echo 'Affiche tous les articles';});
 $router->get('/post/:id-:slug',function($id,$slug)
 {echo $id.' : '.$slug;},'post')->width('id','[0-9]+')->width('slug','[0-9a-z\-]+');
 $router->post('/post/:id-:slug',function($id,$slug){echo 'Poster un commentaire pour l\'article '. $id;});
 $router->get('/login',function(){echo 'Se connecter';});
 $router->get('/legal',function(){echo 'Mentions légales';});
-$router->get('/testRedirect','FrontController#testredirect','testRedirect');
+
 
 
 $router->run();
