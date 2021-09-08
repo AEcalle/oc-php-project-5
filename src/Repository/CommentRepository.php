f52e9f8d937e9249ca7078f2e684a7ad56fa0d21
AEcalle <contact@aurelienecalle.fr>
2021-09-08 10:44:45 +0200

    CommentRepository

---
+++ src/Repository/CommentRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AEcalle\Oc\Php\Project5\Repository;
+
+use AEcalle\Oc\Php\Project5\Entity\Comment;
+use AEcalle\Oc\Php\Project5\Repository\AbstractRepository;
+
+class CommentRepository extends AbstractRepository
+{
+    protected string $table = 'comment';
+
+    public function add(string $content, string $createdAt, string $writer, int $postId): Comment 
+    {
+        $q = parent::$db->prepare("INSERT INTO $this->table(content, created_at, writer, post_id) 
+        VALUES(:content, :created_at, :writer, :post_id)");
+        $q->execute([           
+            ':content' => $content,
+            ':created_at' => $createdAt,
+            ':writer' => $writer,
+            ':post_id' => $postId
+        ]);
+
+        $entity = $this->find(parent::$db->lastInsertId());
+
+        return $entity;
+    }
+
+    public function update(int $id, string $content, string $createdAt, string $writer, int $postId): Comment 
+    {
+        $q = parent::$db->prepare("UPDATE $this->table SET content = :content, created_at = :created_at,
+        writer = :writer, post_id = :post_id WHERE id = :id");
+        $q->execute([
+            ':id' => $id,      
+            ':content' => $content,
+            ':created_at' => $createdAt,
+            ':writer' => $writer,
+            ':post_id' => $postId
+        ]);
+
+        $entity = $this->find($id);
+
+        return $entity;
+    }
+}
